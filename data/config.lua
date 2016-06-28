e02cb7666df8bd236b627ec4d8250dcf7c5a3c8e
SPARKTEA <rodealasade@gmail.com>
2016-06-28 04:30:32 +0300

    Add files via upload

---
+++ data/config.lua
@@ -0,0 +1,37 @@
+do local _ = {
+  disabled_channels = {},
+  enabled_plugins = {
+    "groupcontrol",
+    "membercontrol",
+    "moderation",
+    "help",
+    "About",
+    "info",
+    "link",
+    "bot",
+    "info_help",
+    "id",
+    "id_rebly",
+    "delrebly",
+    "forward",
+    "kicked",
+    "mk_gb",
+    "help_sudo",
+    "infoserver",
+    "rmsg",
+    "bot",
+    "tag",
+    "update",
+    "upgrade",
+    "plugins",
+  },
+  moderation = {
+    data = "data/moderation.json"
+  },
+  sudo_users = {
+    7513052,
+    0
+  }
+}
+return _
+end
